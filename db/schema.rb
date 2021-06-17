8c274f9643a480f3dc632ada7e4ba88f566cd0da
Joshua Charles Seckman <jseckman90@gmail.com>
2021-06-17 16:23:35 -0400

    User sees own todos only

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_235954) do
+ActiveRecord::Schema.define(version: 2021_06_17_192002) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
