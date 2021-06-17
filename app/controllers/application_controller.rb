f07c3022458027ff3c1160004bf4789646ff356c
Joshua Charles Seckman <jseckman90@gmail.com>
2021-06-17 16:59:19 -0400

    Extract session[:current_email] to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,17 @@ def authenticate
 end
 
 def signed_in?
-    session[:current_email].present?
+    current_email.present?
 end
+
+
+def current_email
+    session[:current_email]
+end
+
+def sign_in_as(email)
+    session[:current_email] = email
+end
+
+
 end
